eb1c79895cd24c76b1ca02183b13ff634744b998
Ezequiel Magi <ezequielmaggi@gmail.com>
2022-09-10 17:44:23 -0300

    terrrminada la version mobile

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+const header = document.querySelector('header')
+const advice = document.querySelector('article')
+const URL = "https://api.adviceslip.com/advice"
+
+const getAdvice = async () => {
+    const resp = await fetch(URL)
+    const data = await resp.json()
+    const datos = {data}
+    console.log(datos.data.slip.id)
+    header.innerHTML = `Advice #${datos.data.slip.id}` 
+    console.log(datos.data.slip.advice)
+    advice.innerHTML = `"${datos.data.slip.advice}"`
+}
+
+
+getAdvice()
+
+console.log(header)
+console.log(advice)
