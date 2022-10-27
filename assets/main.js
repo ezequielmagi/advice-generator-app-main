19cbd7f1994683b5cbc08c66f51ab4fb0380184e
Ezequiel Magi <ezequielmaggi@gmail.com>
2022-10-27 17:54:26 -0300

    Modificado innerHTML por textContent como mejora

--- assets/main.js
+++ assets/main.js
@@ -6,14 +6,10 @@ const getAdvice = async () => {
     const resp = await fetch(URL)
     const data = await resp.json()
     const datos = {data}
-    console.log(datos.data.slip.id)
-    header.innerHTML = `Advice #${datos.data.slip.id}` 
+    header.textContent = `Advice #${datos.data.slip.id}` 
     console.log(datos.data.slip.advice)
-    advice.innerHTML = `"${datos.data.slip.advice}"`
+    advice.textContent = `"${datos.data.slip.advice}"`
 }
 
 
 getAdvice()
-
-console.log(header)
-console.log(advice)
